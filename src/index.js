c8cba07ce67465f0de9f80555f7ace3607b44d37
Mariko <maripiyok@gmail.com>
2018-12-06 22:32:31 +0900

    check importing bootstrap success

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import _ from "lodash";
 import './style.sass';
 
 function component() {
-  let element = document.createElement('div');
+  let containerElement = document.createElement('div');
+  containerElement.classList.add('container');
 
-  // Lodash
+  let element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
-  return element;
+  containerElement.appendChild(element);
+  return containerElement;
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,13 @@ module.exports = {
           'css-loader',
           'sass-loader'
         ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   }
