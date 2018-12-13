a795d987c104de8ae878a88b6c6db86a8d62aa17
Mariko <maripiyok@gmail.com>
2018-12-13 22:04:09 +0900

    divide webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,30 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-  mode: 'production',
   entry: {
     app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
-    new webpack.HotModuleReplacementPlugin()
+      title: 'Production'
+    })
   ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
   module: {
     rules: [
       {
@@ -49,5 +36,10 @@ module.exports = {
         ]
       }
     ]
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   }
 };
